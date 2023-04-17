67393ec356687a0592e4b9159122c55cd427cad3
Bjørn Wiegell <bjwi@trifork.com>
2023-04-17 09:18:47 +0200

    cleanup example

--- libs/diabetes/medicine/state/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/examplesub/examplesub.actions'
-export * from './lib/examplesub/examplesub.reducers'
-export * from './lib/examplesub/examplesub.selectors'
-export * from './lib/examplesub/examplesub.effects'
-export * from './lib/medicine.store';

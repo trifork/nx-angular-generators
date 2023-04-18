7d7e2bf155e852ac8df09222768c91ce2645ffdd
Bjørn Wiegell <bjwi@trifork.com>
2023-04-18 15:28:16 +0200

    file generation all the way throug

--- src/generators/component/generator.ts
+++ src/generators/component/generator.ts
@@ -1,5 +1,14 @@
 import { libraryGenerator } from "@nrwl/angular/generators";
-import { formatFiles, getWorkspaceLayout, Tree } from "@nrwl/devkit";
+import {
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  getWorkspacePath,
+  names,
+  offsetFromRoot,
+  Tree,
+  workspaceRoot,
+} from "@nrwl/devkit";
 import {
   scssGenerator,
   configurationGenerator as stylelintConfigGenerator,
@@ -9,89 +18,108 @@ import { pruneCompilerOptions } from "../../utils/pruneCompilerOptions";
 import { generateSourceTagsGeneric, tagsGenerator } from "../tags/generator";
 import { FeatureGeneratorSchema } from "./schema";
 import { changeEslintPrefix } from "../../utils/changeEslintPrefix";
+import path = require("path");
 
-interface NormalizedSchema extends FeatureGeneratorSchema {
-  projectName: string;
+interface NormalizedSchema extends Required<FeatureGeneratorSchema> {
   projectRoot: string;
   projectDirectory: string;
 }
 
 function normalizeOptions(
   tree: Tree,
-  options: FeatureGeneratorSchema
+  options: Required<FeatureGeneratorSchema>,
+  offsetFromProject: String
 ): NormalizedSchema {
-  const { domainName, featureName } = options;
+  const { domainName, componentName, libName, superDomainName } = options;
   const projectDirectory = `${kebabify(options.superDomainName)}/${kebabify(
     domainName
-  )}/${libType}-${featureName}`;
-  const projectName = `${libType}-${featureName}`;
+  )}/${options.libName}`;
   const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
 
   return {
     ...options,
-    projectName,
     projectRoot,
     projectDirectory,
   };
 }
 
-const libType = "feature";
+function addFiles(tree: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: "",
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, "files"),
+    options.projectRoot,
+    templateOptions
+  );
+}
+
+function optionsAreComplete(options: FeatureGeneratorSchema) {
+  return !!options.domainName && !!options.libName && !!options.superDomainName;
+}
+
+function getSuperDomainFromPath(path: string) {
+  const superDomain = path.split(workspaceRoot)[1].split("/")[2];
+  console.log("Deriving superdomain from working directory: ", superDomain);
+  return superDomain;
+}
+
+function getDomainFromPath(path: string) {
+  const domain = path.split(workspaceRoot)[1].split("/")[3];
+  console.log("Deriving domain from working directory: ", domain);
+  return domain;
+}
+
+function getLibFromPath(path: string) {
+  const lib = path.split(workspaceRoot)[1].split("/")[4];
+  console.log("Deriving lib from working directory: ", lib);
+  return lib;
+}
+
+function parseStructureFromWorkingDirectory() {
+  if (process.env["INIT_CWD"] === undefined || process.env["INIT_CWD"] === "")
+    throw new Error("Can not find component out path");
+
+  // Derive working dir from env variable
+  const pathToComponentOutput = process.env["INIT_CWD"];
+
+  // Parse domain etc. from the path above if not supplied
+  const superDomainName = getSuperDomainFromPath(pathToComponentOutput);
+  const domainName = getDomainFromPath(pathToComponentOutput);
+  const libName = getLibFromPath(pathToComponentOutput);
+  if (superDomainName === "" || domainName === "" || libName === "")
+    throw new Error("Parse error");
+
+  return { superDomainName, domainName, libName };
+}
 
 export default async function (tree: Tree, options: FeatureGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  const { projectName, featureName, domainName, projectRoot, superDomainName } =
-    normalizedOptions;
-  const fullResultingProjectName = `${superDomainName}-${domainName}-${libType}-${featureName}`;
-  const selectorPrefix = kebabify(superDomainName);
+  // Establish options
+  let completeOptions: Required<FeatureGeneratorSchema>;
+  if (!optionsAreComplete(options)) {
+    completeOptions = {
+      ...parseStructureFromWorkingDirectory(),
+      componentName: options.componentName,
+    };
+  } else {
+    completeOptions = options as Required<FeatureGeneratorSchema>;
+  }
+  const offsetFromProjectRoot = "/src/lib/components";
 
-  // Generate standard lib
-  const sourceTags = generateSourceTagsGeneric(
-    superDomainName,
-    domainName,
-    libType,
-    featureName
+  // eslint-disable-next-line no-console
+  console.log("Generating component in:", offsetFromProjectRoot);
+
+  // Add template files
+  const normalizedOptions = normalizeOptions(
+    tree,
+    completeOptions,
+    offsetFromProjectRoot
   );
-  await libraryGenerator(tree, {
-    selector: `${kebabify(superDomainName)}-rename-this`,
-    buildable: true,
-    name: projectName,
-    standalone: true,
-    routing: true,
-    lazy: true,
-    displayBlock: true,
-    style: "scss",
-    simpleName: true,
-    directory: superDomainName + "/" + domainName,
-    tags: Object.values(sourceTags).join(),
-  });
+  // TODO: after unit testing is implemented, try and clean up naming variables
+  addFiles(tree, normalizedOptions);
 
-  // Generate stylelint
-  await stylelintConfigGenerator(tree, {
-    project: fullResultingProjectName,
-    skipFormat: false,
-    formatter: "string",
-  });
-
-  // Generate scss
-  await scssGenerator(tree, {
-    project: fullResultingProjectName,
-    skipFormat: false,
-  });
-
-  // Update tags and set rules
-  await tagsGenerator(tree, {
-    superDomainName: options.superDomainName,
-    allowedSubDomainsInShared: ["api-models"],
-    allOfSharedAllowed: true,
-    allowedLibTypesInDomain: ["models", "state", "ui", "util"],
-    domainName,
-    libType,
-    libName: featureName,
-  });
-
-  //   Prune compileroptions from the new tsconfig
-  await pruneCompilerOptions(tree, projectRoot);
-  await changeEslintPrefix(tree, projectRoot, selectorPrefix);
-
-  await formatFiles(tree);
+  //   await formatFiles(tree);
 }

16faf510ffcfcc5574937f381de74996d0e7b027
Bjørn Wiegell <bjwi@trifork.com>
2023-04-19 10:31:59 +0200

    more component generator additions

--- src/generators/component/generator.ts
+++ src/generators/component/generator.ts
@@ -1,5 +1,7 @@
 import { componentGenerator, libraryGenerator } from "@nrwl/angular/generators";
+import { callRule } from "";
 import {
+  convertNxGenerator,
   formatFiles,
   generateFiles,
   getWorkspaceLayout,
@@ -7,6 +9,7 @@ import {
   names,
   offsetFromRoot,
   Tree,
+  updateJson,
   workspaceRoot,
 } from "@nrwl/devkit";
 import {
@@ -20,9 +23,12 @@ import { FeatureGeneratorSchema } from "./schema";
 import { changeEslintPrefix } from "../../utils/changeEslintPrefix";
 import path = require("path");
 import { Schema } from "@nrwl/angular/src/generators/component/schema";
+import { writeFileSync, readFileSync } from "fs";
 
 interface CompleteOptions extends Required<FeatureGeneratorSchema> {
-  offsetFromProject: string;
+  // Null if called with all options, where cwd parsing is not applicable
+  cwdOffsetFromProject: string | null;
+  componentOutPutRelativeToProject: string;
 }
 
 interface NormalizedSchema extends CompleteOptions {
@@ -36,9 +42,9 @@ function normalizeOptions(
   options: CompleteOptions
 ): NormalizedSchema {
   const { domainName, libName, superDomainName } = options;
-  const projectDirectory = `${kebabify(options.superDomainName)}/${kebabify(
-    domainName
-  )}/${options.libName}`;
+  const projectDirectory = `libs/${kebabify(
+    options.superDomainName
+  )}/${kebabify(domainName)}/${options.libName}`;
   const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
   const projectName = `${superDomainName}-${domainName}-${libName}`;
 
@@ -56,19 +62,16 @@ function optionsAreComplete(options: FeatureGeneratorSchema) {
 
 function getSuperDomainFromPath(path: string) {
   const superDomain = path.split(workspaceRoot)[1].split("/")[2];
-  console.log("Deriving superdomain from working directory: ", superDomain);
   return superDomain;
 }
 
 function getDomainFromPath(path: string) {
   const domain = path.split(workspaceRoot)[1].split("/")[3];
-  console.log("Deriving domain from working directory: ", domain);
   return domain;
 }
 
 function getLibFromPath(path: string) {
   const lib = path.split(workspaceRoot)[1].split("/")[4];
-  console.log("Deriving lib from working directory: ", lib);
   return lib;
 }
 
@@ -81,7 +84,6 @@ function getProjectOffsetFromPath(path: string) {
     // eslint-disable-next-line no-console
     console.log("Could not get project offset, defaulting to ''");
   }
-  console.log("Derived project offset from working directory: ", offset);
   return offset;
 }
 
@@ -96,11 +98,57 @@ function parseStructureFromWorkingDirectory() {
   const superDomainName = getSuperDomainFromPath(pathToComponentOutput);
   const domainName = getDomainFromPath(pathToComponentOutput);
   const libName = getLibFromPath(pathToComponentOutput);
-  const offsetFromProject = getProjectOffsetFromPath(pathToComponentOutput);
+  const cwdOffsetFromProject = getProjectOffsetFromPath(pathToComponentOutput);
   if (superDomainName === "" || domainName === "" || libName === "")
     throw new Error("Parse error");
+  console.log(
+    "Deriving superdomain, domain, libname and offset from lib directory: ",
+    superDomainName,
+    domainName,
+    libName,
+    cwdOffsetFromProject
+  );
 
-  return { superDomainName, domainName, libName, offsetFromProject };
+  return {
+    superDomainName,
+    domainName,
+    libName,
+    cwdOffsetFromProject,
+    componentOutPutRelativeToProject: cwdOffsetFromProject,
+  } as Omit<CompleteOptions, "componentName">;
+}
+
+function shouldBeExported(completeOptions: CompleteOptions) {
+  return (
+    completeOptions.libName == "ui" ||
+    completeOptions.domainName === "shared" ||
+    completeOptions.superDomainName === "shared"
+  );
+}
+
+function addExport(
+  completeOptions: CompleteOptions,
+  tree: Tree,
+  normalizedOptions: NormalizedSchema
+) {
+  const path = normalizedOptions.projectDirectory + "/src/index.ts";
+  if (!tree.exists(path)) throw new Error("Could not find index.ts");
+  const oldIndexTS = tree.read(path)!.toString();
+  const newIndexTS = `${oldIndexTS}\nexport * from './${
+    completeOptions.cwdOffsetFromProject?.split("src/")[1]
+  }/${completeOptions.componentName}/${
+    completeOptions.componentName
+  }.component'`;
+  tree.write(path, newIndexTS);
+}
+
+function componentAllowedOnPath(completeOptions: CompleteOptions) {
+  return (
+    completeOptions.libName === "ui" ||
+    completeOptions.libName.includes("feature") ||
+    completeOptions.domainName === "shared" ||
+    completeOptions.superDomainName === "shared"
+  );
 }
 
 export default async function (tree: Tree, options: FeatureGeneratorSchema) {
@@ -114,12 +162,22 @@ export default async function (tree: Tree, options: FeatureGeneratorSchema) {
   } else {
     completeOptions = {
       ...(options as Required<FeatureGeneratorSchema>),
-      offsetFromProject: "/src/lib/components/" + options.componentName,
+      cwdOffsetFromProject: null,
+      componentOutPutRelativeToProject:
+        "/src/lib/components/" + options.componentName,
     };
   }
 
-  // eslint-disable-next-line no-console
-  console.log("Generating component in:", completeOptions.offsetFromProject);
+  // Safeguard component generation in unwanted places
+  if (!componentAllowedOnPath(completeOptions))
+    throw new Error(
+      "Not a good spot for a component! In a generic domain, components must be in ui lib or feature lib"
+    );
+
+  console.log(
+    "Generating component in:",
+    completeOptions.componentOutPutRelativeToProject
+  );
 
   // Run generator with options
   const normalizedOptions = normalizeOptions(tree, completeOptions);
@@ -127,15 +185,21 @@ export default async function (tree: Tree, options: FeatureGeneratorSchema) {
     name: normalizedOptions.componentName,
     project: normalizedOptions.projectName,
     standalone: true,
-    selector: kebabify(normalizedOptions.componentName),
     displayBlock: true,
     style: "scss",
     prefix:
       normalizedOptions.superDomainName === "shared"
         ? "sam-"
-        : normalizedOptions.domainName,
+        : normalizedOptions.superDomainName,
   };
-  componentGenerator(tree, angularGeneratorOptions);
+  // This usage generates warning, that it should be wrapped in
+  // callRule. This is not possible atm. bc. of unaligned type Tree
+  // between the @angular-devkit/schematics package and @nrwl/devkit
+  // TODO: should be checked later, when packages are upgraded
 
-  //   await formatFiles(tree);
+  await componentGenerator(tree, angularGeneratorOptions);
+
+  if (shouldBeExported(completeOptions))
+    addExport(completeOptions, tree, normalizedOptions);
+  await formatFiles(tree);
 }
